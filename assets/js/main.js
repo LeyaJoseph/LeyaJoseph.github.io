4d845648937fec2fa18382da49bdfc5ab0751537
LeyaJoseph <119883190+LeyaJoseph@users.noreply.github.com>
2025-12-27 20:10:51 -0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,3 +32,40 @@
 		});
 
 })(jQuery);
+
+document.addEventListener('DOMContentLoaded', function() {
+    const modal = document.getElementById('project-modal');
+    const modalContent = document.getElementById('modal-project-content');
+    const closeBtn = document.querySelector('.close');
+    const viewDetailsBtns = document.querySelectorAll('.view-details-btn');
+
+    viewDetailsBtns.forEach(btn => {
+        btn.addEventListener('click', function() {
+            const projectCard = this.closest('.project-card');
+            const projectDetails = projectCard.querySelector('.project-details').innerHTML;
+            const projectTitle = projectCard.querySelector('h4').textContent;
+            const projectImage = projectCard.querySelector('img').outerHTML;
+            const projectBrief = projectCard.querySelector('.project-brief').innerHTML; 
+            const projectTools = projectCard.querySelector('.project-tools').textContent;
+
+            modalContent.innerHTML = `
+                <h3>${projectTitle}</h3>
+                ${projectImage}
+                <div class="modal-brief"><strong>Summary: </strong>${projectBrief}</div>
+                <div class="modal-tools"><strong>Tools Used: </strong> ${projectTools}</div>
+                <div class="modal-details"><strong>Details:</strong>${projectDetails}</div>
+            `;
+            modal.style.display = 'block';
+        });
+    });
+
+    closeBtn.onclick = function() {
+        modal.style.display = 'none';
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+});
