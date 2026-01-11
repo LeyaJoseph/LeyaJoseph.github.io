dca52ad6b08943286b5cab2a7e058e43ec3721be
LeyaJoseph <119883190+LeyaJoseph@users.noreply.github.com>
2026-01-11 09:52:44 -0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,9 +63,10 @@ document.addEventListener('DOMContentLoaded', function() {
         modal.style.display = 'none';
     }
 
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = 'none';
-        }
-    }
+    modal.addEventListener('pointerdown', function (event) {
+    	if (event.target === modal) {
+        	modal.style.display = 'none';
+    	}
+    });
+
 });
